cf817023177b29781fd4a9d365cbf6b372326de8
Thomas Akam <4661184+ThomasAkam@users.noreply.github.com>
2014-06-27 23:44:44 +0100

    - Added framework_run function.
    - Automatically add entry and exit events to agent event dictionaries.
    
    - Added agent reset function.
    
    - Minor modifications to blinker.

--- State_machine.py
+++ State_machine.py
@@ -8,14 +8,23 @@ class State_machine():
 
     def __init__(self):
         self.ID = register_machine(self) # Register with framework.
-        self.event_queue = Event_queue()
+        self.event_queue = Event_queue() # Create event que.
+        # Setup event dictionaries:
+        self.events['entry'] = -1 # add entry and exit events to dictionary.
+        self.events['exit' ] = -2 
+        self._ID2event = {ID:event for event, ID 
+                                   in self.events.items()}
+        self.reset()
+
+    def reset(self):
+        # Resets agent to initial state, with single entry event in que.
         self.current_state = self.initial_state
-         # Reverse dictionary for mapping event names to ID numbers.
-        self._inv_events = {ID:event for event, ID in self.events.items()}
+        self.event_queue.reset()  
+        self.event_queue.put(self.events['entry']) 
 
     def update(self):
         if self.event_queue.available():
-            self.process_event(self._inv_events[self.event_queue.get()[0]])
+            self.process_event(self._ID2event[self.event_queue.get()[0]])
 
     def set_timer(self, event, interval):
         timer.set(self.events[event], int(interval))
@@ -27,8 +36,6 @@ class State_machine():
         self.process_event('exit')
         self.current_state = next_state
         self.process_event('entry')
-
-
 
 
 # ----------------------------------------------------------------------------------------
@@ -50,19 +57,21 @@ class blinker(State_machine):
         if   self.current_state == 'LED_on':
 
             if event == 'entry':
+                self.set_timer('timer_evt', 1 * second)
                 print('LED_on')
 
             elif event == 'exit':
                 print('LED_off')
 
             elif event == 'timer_evt':
-                self.set_timer('timer_evt', 1 * second)
                 self.goto_state('LED_off')
-
 
         elif self.current_state == 'LED_off':
 
-            if event == 'timer_evt':
+            if event == 'entry':
                 self.set_timer('timer_evt', 0.5 * second)
+
+            if event == 'timer_evt':
                 self.goto_state('LED_on')
+
 
