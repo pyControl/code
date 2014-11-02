d9e9c47f1f262c0356c5183ebd4797a1a0280809
Thomas Akam <thomas.akam@gmail.com>
2014-11-02 10:36:01 +0000

    State machine constructor must now be passed framwork module, removing need for seperate call to register machines.

--- script.py
+++ script.py
@@ -1,19 +1,15 @@
-from PyControl import *
+import PyControl as pc
 from examples import *
 
 # Instantiate state machines.
 
-blinker_1 = blinker(LED = 1, period = 1)      
-blinker_2 = blinker(LED = 2, period = 0.5)     
-blinker_3 = blinker(LED = 3, period = 0.25) 
-blinker_4 = blinker(LED = 4, period = 0.125) 
-
-# Register machiness with PyControl.
-register([blinker_1, blinker_2, blinker_3, blinker_4]) 
-
+blinker_1 = blinker(pc, LED = 1, period = 1)      
+blinker_2 = blinker(pc, LED = 2, period = 0.5)     
+blinker_3 = blinker(pc, LED = 3, period = 0.25) 
+blinker_4 = blinker(pc, LED = 4, period = 0.125) 
 
 # Run PyControl.
-run_machines(5 * second)  
+pc.run_machines(5 * second)  
 
 
 
