3dc55fca237054c5f6e77c9221115c80c87bc006
Thomas Akam <4661184+ThomasAkam@users.noreply.github.com>
2014-11-29 14:00:47 +0000

    - Added basic serial data output functionality.

--- State_machine.py
+++ State_machine.py
@@ -24,8 +24,8 @@ class State_machine():
 
         self.check_valid_IDs()
 
-        self._ID2event = {ID:event for event, ID # Dict mapping IDs to event names.
-                                   in self.events.items()}
+        self._ID2name = {ID:event for event, ID # Dict mapping IDs to event names.
+                                   in list(self.events.items()) + list(self.states.items())}
 
         self.pc = PyControl # Pointer to framework.
         self.ID  = self.pc.register_machine(self)
@@ -48,7 +48,7 @@ class State_machine():
 
     def process_event_ID(self, event_ID):
         # Process event given event ID
-        self.process_event(self._ID2event[event_ID])
+        self.process_event(self._ID2name[event_ID])
 
     def process_event(self, event):
         # Process event given event name. Overwrite with desired
@@ -59,16 +59,17 @@ class State_machine():
         self.process_event('exit')
         self.current_state = next_state
         self.process_event('entry')
+        self.pc.data_output_queue.put((self.ID, self.states[next_state], self.pc.current_time))
 
     def check_valid_IDs(self):
         # Check that there are no repeated state or events IDs.
-        all_IDs = list(self.events.values()) + list(self.states.values())
-        unique_IDs = set(all_IDs)
-        if len(unique_IDs) < len(all_IDs):
+        all_ID2name = list(self.events.values()) + list(self.states.values())
+        unique_ID2name = set(all_ID2name)
+        if len(unique_ID2name) < len(all_ID2name):
             print('Error: State and event IDs must be unique.')
-            
 
-    def print_IDs(self):
+
+    def print_ID2name(self):
         # Print event and state dictionaries.
         print('Events:')
         for event in self.events:
