12d7631f3e55e683f74e5cd84bbe50f134fc483d
Thomas Akam <thomas.akam@gmail.com>
2014-12-18 23:33:55 +0000

    - Modified how state machine functionality is defined by replacing single process event function with set of individual state event handler functions which each describe behaviour in a single state.
    - Improved state machine event printing.
    
    - State machine events and states can now be provided as lists or dicts.  If
    lists are provided, IDs are automatically generated.

--- two_step.py
+++ two_step.py
@@ -2,25 +2,25 @@ from State_machine import *
 
 class Two_step(State_machine):
 
-    states = {'pre_session'       : 1,
-              'post_session'      : 2,
-              'center_active'     : 3,
-              'left_active'       : 4,
-              'right_active'      : 5,
-              'left_reward'       : 6,
-              'right_reward'      : 7,
-              'wait_for_poke_out' : 8,
-              'inter_trial'       : 9}
+    states = ['pre_session',
+              'post_session',
+              'center_active',
+              'left_active',
+              'right_active',
+              'left_reward',
+              'right_reward',
+              'wait_for_poke_out',
+              'inter_trial']
 
-    events = {'left_poke'        : 10, 
-              'left_poke_out'    : 11,
-              'right_poke'       : 12,
-              'right_poke_out'   : 13,
-              'high_poke'        : 14,
-              'low_poke'         : 15,
-              'session_timer'    : 16,
-              'state_timer'      : 17,
-              'session_startstop': 18}
+    events = ['left_poke', 
+              'left_poke_out',
+              'right_poke',
+              'right_poke_out',
+              'high_poke',
+              'low_poke',
+              'session_timer',
+              'state_timer',
+              'session_startstop']
 
     initial_state = 'pre_session'
 
@@ -33,123 +33,94 @@ class Two_step(State_machine):
 
         State_machine.__init__(self, PyControl, box)
 
-    def process_event(self, event):
-
-        # Session starting and stopping behaviour.
-
-        if event == 'session_startstop':
-            if self.state == 'pre_session':
-                self.box.houselight.on()
-                self.goto('center_active')
-            else:
-                self.goto('post_session')
-
-        elif self.state == 'post_session':
-
-            if event == 'entry':
-                self.box.houselight.off()     
-
-        # Within trial event processing.
-
-
-        elif self.state == 'center_active':
-
-            if event == 'entry':
-                self.box.center_poke.LED.on()
-
-            elif event == 'exit':
-                self.box.center_poke.LED.off()
-
-            elif event == 'high_poke':
-                if withprob(self.norm_prob):
-                    self.goto('left_active')
-                else:
-                    self.goto('right_active')
-
-            elif event == 'low_poke':
-                if withprob(self.norm_prob):
-                    self.goto('right_active')
-                else:
-                    self.goto('left_active')
-
-
-        elif self.state ==  'left_active':
-
-            if event == 'entry':
-                self.box.left_poke.LED.on()
-
-            if event == 'exit':
-                self.box.left_poke.LED.off()
-
-            if event == 'left_poke':
-                if withprob(self.reward_probs[0]):
-                    self.goto('left_reward')
-                else:
-                    self.goto('wait_for_poke_out')
-
-
-        elif self.state ==  'right_active':
-
-            if event == 'entry':
-                self.box.right_poke.LED.on()
-
-            if event == 'exit':
-                self.box.right_poke.LED.off()
-
-            if event == 'right_poke':
-                if withprob(self.reward_probs[1]):
-                    self.goto('right_reward')
-                else:
-                    self.goto('wait_for_poke_out')
-
-
-        elif self.state == 'left_reward':
-
-            if event == 'entry':
-                self.box.left_poke.SOL.on()
-                self.set_timer('state_timer', 100 * ms)
-
-            if event == 'exit':
-                self.box.left_poke.SOL.off()
-
-            if event == 'state_timer':
-                self.goto('wait_for_poke_out')
-
-
-        elif self.state ==  'right_reward':
-
-            if event == 'entry':
-                self.box.right_poke.SOL.on()
-                self.set_timer('state_timer', 100 * ms)
-
-            if event == 'exit':
-                self.box.right_poke.SOL.off()
-
-            if event == 'state_timer':
-                self.goto('wait_for_poke_out')     
-            
-
-        elif self.state == 'wait_for_poke_out':
-
-            if event == 'entry':
-                if not (self.box.left_poke.get_state() or \
-                        self.box.right_poke.get_state()):
-                    self.goto('inter_trial') # Subject already left poke.
-
-            if event in ['left_poke_out', 'right_poke_out']:
-                self.goto('inter_trial')
-
-
-        elif self.state == 'inter_trial':
-
-            if event == 'entry':
-                self.set_timer('state_timer', 1 * second)
-
-            if event == 'state_timer':
-                self.goto('center_active')
-
     def stop(self):
         self.box.off()
+
+    # State event handler functions.
+
+    def all_states(self, event):
+        if event == 'session_startstop' and not self.state == 'pre_session':
+            self.goto('post_session')
+
+    def pre_session(self, event):
+        if event == 'session_startstop':
+            self.box.houselight.on()
+            self.goto('center_active')
+
+    def post_session(self, event):
+            self.box.houselight.off()     
+
+    def center_active(self, event):
+        if event == 'entry':
+            self.box.center_poke.LED.on()
+        elif event == 'exit':
+            self.box.center_poke.LED.off()
+        elif event == 'high_poke':
+            if withprob(self.norm_prob):
+                self.goto('left_active')
+            else:
+                self.goto('right_active')
+        elif event == 'low_poke':
+            if withprob(self.norm_prob):
+                self.goto('right_active')
+            else:
+                self.goto('left_active')
+
+    def left_active(self, event):
+        if event == 'entry':
+            self.box.left_poke.LED.on()
+        elif event == 'exit':
+            self.box.left_poke.LED.off()
+        elif event == 'left_poke':
+            if withprob(self.reward_probs[0]):
+                self.goto('left_reward')
+            else:
+                self.goto('wait_for_poke_out')
+
+    def right_active(self, event):
+        if event == 'entry':
+            self.box.right_poke.LED.on()
+        elif event == 'exit':
+            self.box.right_poke.LED.off()
+        elif event == 'right_poke':
+            if withprob(self.reward_probs[1]):
+                self.goto('right_reward')
+            else:
+                self.goto('wait_for_poke_out')
+
+    def left_reward(self, event):
+        if event == 'entry':
+            self.box.left_poke.SOL.on()
+            self.set_timer('state_timer', 100 * ms)
+        elif event == 'exit':
+            self.box.left_poke.SOL.off()
+        elif event == 'state_timer':
+            self.goto('wait_for_poke_out')
+
+    def right_reward(self, event):
+        if event == 'entry':
+            self.box.right_poke.SOL.on()
+            self.set_timer('state_timer', 100 * ms)
+        if event == 'exit':
+            self.box.right_poke.SOL.off()
+        if event == 'state_timer':
+            self.goto('wait_for_poke_out')     
+        
+    def wait_for_poke_out(self, event):
+        if event == 'entry':
+            if not (self.box.left_poke.get_state() or \
+                    self.box.right_poke.get_state()):
+                self.goto('inter_trial') # Subject already left poke.
+        elif event in ['left_poke_out', 'right_poke_out']:
+            self.goto('inter_trial')
+
+    def inter_trial(self, event):
+        if event == 'entry':
+            self.set_timer('state_timer', 1 * second)
+        if event == 'state_timer':
+            self.goto('center_active')
+
+
 
 
 
