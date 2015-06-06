969206e07ddd202bd085a2f2d593d92cab5904a1
Thomas Akam <4661184+ThomasAkam@users.noreply.github.com>
2015-06-06 13:13:56 +0100

    Improved digital input debouncing.

--- pyControl/hardware.py
+++ pyControl/hardware.py
@@ -7,34 +7,31 @@ from . import framework as fw
 
 class Digital_input():
     def __init__(self, pin, rising = None, falling = None,
-                 debounce = 10, pull = pyb.Pin.PULL_DOWN):
+                 debounce = 5, pull = pyb.Pin.PULL_DOWN):
         # Digital_input class provides functionallity to generate framework events when a
         # specified pin on the Micropython board changes state. Seperate events can be
         # specified for rising and falling pin changes. The event names associated with
-        # rising and falling edges are specified when Digital_input is initialised,
-        # these are converted to the appropriate event IDs when the Digital_input is 
-        # assigned to a state machine.  By defalt debouncing is used to prevent multiple
-        # events from being genrated by interrupts occuring very close together.  This is 
-        # implemented by setting the debounce_active flag to true when an event is generated
-        # and setting a timer to elapse after the debounce duration.  When the timer elapses,
-        # the debounce_active flag is set to false, and the pin state is checked to see if 
-        # has changed during the debounce period, in which case the relevent event is generated.
-        # This ensures that transients which are shorter than the debounce time still generate
-        # rising and falling events.
+        # rising and falling edges are specified when Digital_input is initialised using 
+        # the rising and falling arguments.  These are converted to the appropriate event
+        # IDs when the Digital_input is assigned to a state machine.  
+        # By defalt debouncing is used to prevent multiple events being triggered very 
+        # close together in time if the edges are not clean.  The debouncing method used
+        # ensures that transient inputs shorter than the debounce duration still generate 
+        # rising and faling edges.  
         #Arguments:
-        # pin - Name of Micropython pin: e.g. 'Y1'
-        # rising - Name of event triggered on rising edges.
-        # falling - Name of event triggered on falling edges.
-        # debounce - minimum time interval between events (ms), set to False to deactive debouncing.
-        # pull - Used to enable pullup or pulldown resitors on pin.
+        # pin      - Name of Micropython pin: e.g. 'Y1'
+        # rising   - Name of event triggered on rising edges.
+        # falling  - Name of event triggered on falling edges.
+        # debounce - Minimum time interval between events (ms), 
+        #            set to False to deactive debouncing.
+        # pull     - Used to enable pullup or pulldown resitors on pin.
 
         self.rising_event = rising
         self.falling_event = falling
         self.debounce = debounce             
         self.pin = pyb.Pin(pin, pyb.Pin.IN)  # Micropython pin object.
-        pyb.ExtInt(pin, pyb.ExtInt.IRQ_RISING_FALLING, pull, self._ISR) # Configure interrupt on pin.
-        self.interrupt_timestamp = 0         # Time interrupt occured.
-        self.interrupt_rising = False        # True for rising interrupt, false for falling interrupt.
+        pyb.ExtInt(pin, pyb.ExtInt.IRQ_RISING_FALLING, pull, self._ISR) # Configure interrupt.
+        self.interrupt_timestamp = 0
         self.ID = fw.register_hardware(self) # Register Digital_input with framwork.
         self.reset()
 
@@ -52,51 +49,49 @@ class Digital_input():
 
     def _ISR(self, line):
         # Interrupt service routine called on pin change.
-        self.pin_state = self.pin.value()
-        self.interrupt_timestamp = pyb.millis()
         if self.debounce_active:
-           return # Ignore interrupt as too soon after previous interrupt.
-        if self.pin_state and self.rising_event_ID: # Pin is high, rising event.
-            self.interrupt_rising = True
-        elif not self.pin_state and self.falling_event_ID: # Pin is low, falling event.
-            self.interrupt_rising = False
-        else:
-            return # Ignore interrupt as no event_ID assigned to edge.
-        if self.debounce: 
+                return # Ignore interrupt as too soon after previous interrupt.
+        self.interrupt_timestamp = pyb.millis()
+        if self.debounce: # Digitial input uses debouncing.
+            self.pin_state = not self.pin_state
             self.debounce_active = True
+        else:
+            self.pin_state = self.pin.value()
         self.interrupt_triggered = True    # Set tag on Digital_input.
-        fw.interrupts_waiting    = True  # Set tag on framework (common to all Digital_inputs).
+        fw.interrupts_waiting    = True    # Set tag on framework (common to all Digital_inputs).
 
     def _process_interrupt(self):
         # Put apropriate event for interrupt in event queue.
         timestamp = self.interrupt_timestamp - fw.start_time
         self.interrupt_triggered = False
-        if self.interrupt_rising:
-            fw.publish_event((self.machine_ID, self.rising_event_ID, timestamp))
-        else: 
-            fw.publish_event((self.machine_ID, self.falling_event_ID, timestamp))
-        if self.debounce:
-            fw.timer.set(self.ID, self.debounce, -2) # Set timer to trigger _deactivate_debounce after self.debounce milliseconds.
+        self._publish_if_edge_has_event(timestamp)
+        if self.debounce: # Set timer to deactivate debounce in self.debounce milliseconds.
+            fw.timer.set(self.ID, self.debounce, -2) 
 
     def _deactivate_debounce(self):
-        # Called when debounce timer elapses, deactivates debounce state and 
-        # if necessary publishes event that was missed during debounce.
-        if self.interrupt_rising and (not self.pin.value()) and self.falling_event_ID: 
-            # Interrupt was rising but pin is now low - falling event was missed during debounce.
-            fw.publish_event((self.machine_ID, self.falling_event_ID, fw.current_time)) 
-        elif (not self.interrupt_rising) and self.pin.value() and self.rising_event_ID: 
-            # Interrupt was falling but pin is now high - rising event was missed during debounce.
-            fw.publish_event((self.machine_ID, self.rising_event_ID, fw.current_time))
+        # Called when debounce timer elapses, deactivates debounce and 
+        # if necessary publishes event for edge missed during debounce.
+        if not (self.pin_state == self.pin.value()):  # An edge has been missed.
+            self.pin_state = not self.pin_state
+            self._publish_if_edge_has_event(fw.current_time)
         self.debounce_active = False
+
+    def _publish_if_edge_has_event(self, timestamp):
+        # Publish event if detected edge has event ID assigned.
+        if self.pin_state and self.rising_event_ID:          # Rising edge.
+            fw.publish_event((self.machine_ID, self.rising_event_ID, timestamp))
+        elif (not self.pin_state) and self.falling_event_ID: # Falling edge.
+            fw.publish_event((self.machine_ID, self.falling_event_ID, timestamp))
 
     def value(self):
         # Return state of the input. 
         return self.pin.value()
 
     def reset(self): # Reset state of input, called at beginning of run.
-        self.interrupt_triggered = False        # Flag to tell framework to run _process_interrupt.
-        self.debounce_active = False            # Set true when pin is ignoring inputs due to debounce.
+        self.interrupt_triggered = False  # Flag to tell framework to run _process_interrupt.
+        self.debounce_active = False      # Set true when pin is ignoring inputs due to debounce.
         self.pin_state = self.pin.value()
+
 # ----------------------------------------------------------------------------------------
 # Digital Output.
 # ----------------------------------------------------------------------------------------
@@ -143,7 +138,8 @@ ports = {1: {'DIO_A': 'Y1',   # Pin mappings for pyControl devboard 1.0 board.
              'POW_B': 'Y11'}}
 
 class Poke():
-    def __init__(self, port, rising = None, falling = None, rising_B = None, falling_B = None, debounce = 10):
+    def __init__(self, port, rising = None, falling = None, rising_B = None,
+                 falling_B = None, debounce = 5):
 
         self.SOL     = Digital_output(ports[port]['POW_A'])
         self.LED     = Digital_output(ports[port]['POW_B'])
@@ -151,8 +147,6 @@ class Poke():
         self.input_B = Digital_input(ports[port]['DIO_B'], rising_B, falling_B, debounce = debounce)
 
     def set_machine(self, state_machine):
-        # Assigns poke to state machine.  For each input with an event name assigned, adds appropriate event codes, 
-        #  and state_maching ID to hwo object active_pins dictionary.
         self.input_A.set_machine(state_machine)
         self.input_B.set_machine(state_machine)
 
@@ -175,7 +169,8 @@ class Box():
         # Instantiate components.
         self.left_poke   = Poke(port = 1, rising = 'left_poke', falling = 'left_poke_out')
         self.center_poke = Poke(port = 2, rising = 'high_poke', rising_B = 'low_poke')
-        self.right_poke  = Poke(port = 3, rising = 'right_poke', falling = 'right_poke_out', rising_B = 'session_startstop')
+        self.right_poke  = Poke(port = 3, rising = 'right_poke', falling = 'right_poke_out',
+                                rising_B = 'session_startstop')
         self.houselight  = self.center_poke.SOL
 
         self.all_inputs = [self.left_poke, self.center_poke, self.right_poke]
