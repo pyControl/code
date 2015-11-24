213e24aea48c3c0b65c67ad6000a3eb64e39d8bb
Thomas Akam <4661184+ThomasAkam@users.noreply.github.com>
2015-11-24 13:09:00 +0000

    - Added example config and hardware definiton files.

---
+++ host/config/example config/experiments.py
@@ -0,0 +1,16 @@
+from experiment  import Experiment
+from config.calibration import calibration
+
+example_exp = Experiment(
+          name = 'Example_experiment',    
+          start_date = '2015-11-15',
+          subjects = {1:  'm001'},
+          task = 'two_step',
+          hardware = 'Four_pokes_dvb',
+          set_variables = {'outcome_generator.settings': {'first_session'       :  False,
+                                                          'high_trans_contrast' :  False,
+                                                          'high_reward_contrast':  False},
+                           'reward_delivery_durations':calibration['main']},
+          persistent_variables = ['outcome_generator.state']
+          )
+
