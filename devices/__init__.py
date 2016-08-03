100a8904f192d6bc3fbb57c306cb3cc110f4d30e
Thomas Akam <4661184+ThomasAkam@users.noreply.github.com>
2016-08-03 23:00:14 +0100

    - Moved devices classes to seperate folder with individual files corresponding to each board.
    - Substantial simplification and rationalisation of how hardware definitions
    are written.  Devices no longer have seperate connect method, instead pin or
    port is passed to constructor.  Added option for specifying default pullup /
    pulldown for pins which simplifies allowing the same hardware definition
    to work across devboard and breakout board.  Now using classes rather than
    dictionaries to specify devboard and breakout board pin maps.
    
    - Added method to pycboard to put the pyboard into DFU mode for firmware update.
    
    - Modified timing to use pby.millis() rather than incrementing a counter on each
    clock tick as the interrupt which updates millis has the highest priority and
    hence should be less prone to being disrupted.
    
    - Added pulse() method to Digital_output which turns on square wave generated
    by a hardware timer.
    
    - Modified Digital_input to only generate interrupts on rising and falling edges
    if events are defined on rising and falling edges and used by current state
    machine.  This should reduce overheads associated with input processing.
    
    - Modified Digital_input to support a decimate mode where only 1 in every n
    input pules generates a framework event.  This should allow reasonably high
    frequency inputs such as camera sync pulses to be processed with less overhead
    and generate more managable numbers of events in the data file.

---
+++ devices/__init__.py
@@ -0,0 +1,7 @@
+from pyControl.hardware import Digital_input, Digital_output, off
+from devices._poke import Poke
+from devices._double_poke import Double_poke
+from devices._twin_poke import Twin_poke
+from devices._quad_poke import Quad_poke
+from devices._breakout_1_0 import Breakout_1_0
+from devices._devboard_1_0 import Devboard_1_0
