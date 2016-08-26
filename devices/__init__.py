55bed76077e60a0e1e3ae88b5efa15cb943d81e2
takam <takam@localhost>
2016-08-26 18:03:00 +0100

    - Modified audio.py to use a class for an audio output.
    - Added breakout_1_1 specification to devices.

--- devices/__init__.py
+++ devices/__init__.py
@@ -5,3 +5,4 @@ from devices._twin_poke import Twin_poke
 from devices._quad_poke import Quad_poke
 from devices._breakout_1_0 import Breakout_1_0
 from devices._devboard_1_0 import Devboard_1_0
+from devices._breakout_1_1 import Breakout_1_1
--- pyControl/audio.py
+++ pyControl/audio.py
@@ -1,93 +1,83 @@
 import pyb
 import math
 
-_dacs = (pyb.DAC(1), pyb.DAC(2)) # Digital to analog converters.
-_timers = [pyb.Timer(1),pyb.Timer(2)] # Timers used to control pulsing etc.
-
 _sine_len = 100 # Number of points in sine wave.
 _sine_buf = bytearray([128+int(127*math.sin(2*math.pi*i/_sine_len)) for i in range(_sine_len)])
 
 _sqr_buf = bytearray([255,0])
 
-# Variables used for timer updated sounds.
-
-_playing   = [False, False]
-_func      = [None, None]
-_freq      = [0, 0]
-_freq_ind =  [0, 0]
-
 #-----------------------------------------------------------------------------------
-# User functions
+# Audio output
 #-----------------------------------------------------------------------------------
 
-def off(chnl=None):
-    # Stop sound playing, if no channel is specified stops both channels.
-    if chnl is None:
-        off(0)
-        off(1)
-    else:
-        _dacs[chnl].write(127)
-        _timers[chnl].deinit()
-        _playing[chnl] = False
+class Audio_output():
+    def __init__(self, channel=1):
+        assert channel in [1,2], 'channel number invalid, must be 1 or 2.'
+        self._DAC = pyb.DAC(channel) 
+        self._timer = pyb.Timer(channel)
+        self._playing = False
+        self._func = None # Function currently being used for sweeped sound (sine, square or noise)
+        self._freq = 0
+        self._freq_ind = 0
 
-def sine(freq, chnl=0):
-    # Play a sine wave tone at the specified frequency on the specified channel.
-    _dacs[chnl].write_timed(_sine_buf, freq*_sine_len, mode=pyb.DAC.CIRCULAR)
+    # User functions-------------------------------------------------------------------
 
-def square(freq, chnl=0):
-    # Play a square wave tone at the specified frequency on the specified channel.
-    _dacs[chnl].write_timed(_sqr_buf, freq*2, mode=pyb.DAC.CIRCULAR)    
+    def off(self):
+            self._DAC.write(127)
+            self._timer.deinit()
+            self._playing = False
 
-def pulsed_square(freq, pulse_rate, chnl = 0):
-    _pulsed_sound(freq, pulse_rate, square, chnl)
+    def sine(self, freq):  # Play a sine wave tone at the specified frequency.
+        self._DAC.write_timed(_sine_buf, freq*_sine_len, mode=pyb.DAC.CIRCULAR)
 
-def pulsed_sine(freq, pulse_rate, chnl = 0):
-    _pulsed_sound(freq, pulse_rate, sine, chnl)
+    def square(self, freq): # Play a square wave tone at the specified frequency.
+        self._DAC.write_timed(_sqr_buf, freq*2, mode=pyb.DAC.CIRCULAR)    
 
-def sine_sweep(start_freq, end_freq, n_steps, step_rate, chnl = 0):
-    _sound_sweep(start_freq, end_freq, n_steps, step_rate, sine, chnl)
+    def noise(self, freq=10000): # Play white noise with specified maximum frequency.
+        self._DAC.noise(freq*2)
 
-def square_sweep(start_freq, end_freq, n_steps, step_rate, chnl = 0):
-    _sound_sweep(start_freq, end_freq, n_steps, step_rate, square, chnl)
+    def pulsed_sine(self, freq, pulse_rate):
+        self._pulsed_sound(freq, pulse_rate, self.sine)
 
-#-----------------------------------------------------------------------------------
-# Support functions
-#-----------------------------------------------------------------------------------
+    def pulsed_square(self, freq, pulse_rate):
+        self._pulsed_sound(freq, pulse_rate, self.square)
 
-def _pulsed_sound(freq, pulse_rate, func, chnl):
-    global _playing, _freq, _func
-    _freq[chnl] = freq
-    _func[chnl] = func
-    _timers[chnl].init(freq=2*pulse_rate)
-    _timers[chnl].callback(_toggle_sound)
+    def pulsed_noise(self, freq=10000, pulse_rate=10):
+        self._pulsed_sound(freq, pulse_rate, self.noise)
 
-def _toggle_sound(timer):
-    global _playing
-    chnl = int(timer is _timers[1])
-    if _playing[chnl]:
-        _dacs[chnl].write(127)
-        _playing[chnl] = False
-    else:
-        _func[chnl](_freq[chnl], chnl)
-        _playing[chnl] = True
+    def sweeped_sine(self, start_freq, end_freq, n_steps, step_rate):
+        self._sound_sweep(start_freq, end_freq, n_steps, step_rate, self.sine)
 
-def _sound_sweep(start_freq, end_freq, n_steps, step_rate, func, chnl):
-    global _playing, _freq, _freq_ind 
-    freq_ratio = (end_freq/start_freq)**(1./(n_steps-1))
-    _freq[chnl] = [int(start_freq * (freq_ratio**i)) for i in range(n_steps)]
-    _freq_ind[chnl] = 0
-    _func[chnl] = func
-    _timers[chnl].init(freq=step_rate)
-    _timers[chnl].callback(_step_sound)
+    def sweeped_square(self, start_freq, end_freq, n_steps, step_rate):
+        self._sound_sweep(start_freq, end_freq, n_steps, step_rate, self.square)
 
-def _step_sound(timer):
-    global _freq_ind
-    chnl = int(timer is _timers[1])
-    _func[chnl](_freq[chnl][_freq_ind[chnl]], chnl)
-    _freq_ind[chnl] = (_freq_ind[chnl]+1) % len(_freq[chnl])
+    def sweeped_noise(self, start_freq, end_freq, n_steps, step_rate):
+        self._sound_sweep(start_freq, end_freq, n_steps, step_rate, self.noise)
 
+    # Support functions-----------------------------------------------------------------
 
+    def _pulsed_sound(self, freq, pulse_rate, func):
+        self._freq = freq
+        self._func = func
+        self._timer.init(freq=2*pulse_rate)
+        self._timer.callback(self._toggle_sound)
 
+    def _toggle_sound(self, timer):
+        if self._playing:
+            self._DAC.write(127)
+            self._playing = False
+        else:
+            self._func(self._freq)
+            self._playing = True
 
+    def _sound_sweep(self, start_freq, end_freq, n_steps, step_rate, func):
+        freq_ratio = (end_freq/start_freq)**(1./(n_steps-1))
+        self._freq = [int(start_freq * (freq_ratio**i)) for i in range(n_steps)]
+        self._freq_ind = 0
+        self._func = func
+        self._timer.init(freq=step_rate)
+        self._timer.callback(self._step_sound)
 
-
+    def _step_sound(self, timer): # Timer callback to increment frequency during sweeped sounds.
+        self._func(self._freq[self._freq_ind])
+        self._freq_ind = (self._freq_ind+1) % len(self._freq)
