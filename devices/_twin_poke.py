855968b3c2e3719d55c555daf1de2f08286e0092
takam <4661184+ThomasAkam@users.noreply.github.com>
2016-09-02 16:18:55 +0100

    - Fixed a but in hardware.py whereby if digital_inputs were defined in hardware definition but not used in the state machine, the ID numbers would not be assigned correctly.
    - Fixed but with twin_poke class

--- devices/_twin_poke.py
+++ devices/_twin_poke.py
@@ -12,6 +12,6 @@ class Twin_poke():
     # Two IR beams, each with their own LED.
     def __init__(self, port, rising_event_A = None, falling_event_A = None,
                  rising_event_B = None, falling_event_B = None, debounce = 5):
-        self.poke_A = _Poke(port_1.DIO_A, port_1.POW_A, rising_event_A, falling_event_A, debounce)
-        self.poke_B = _Poke(port_1.DIO_B, port_1.POW_B, rising_event_A, falling_event_A, debounce)
+        self.poke_A = _Poke(port.DIO_A, port.POW_A, rising_event_A, falling_event_A, debounce)
+        self.poke_B = _Poke(port.DIO_B, port.POW_B, rising_event_B, falling_event_B, debounce)
         self.LED = Digital_output_group([self.poke_A.LED, self.poke_B.LED])
