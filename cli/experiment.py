614472df963d10f729b0d301f977452c7495fcb0
takam <4661184+ThomasAkam@users.noreply.github.com>
2017-03-10 14:05:11 +0000

    - Changed data file names to include start time.
    - Improved information output to data file before run start.
    
    - Changed post run file transfer to use seperate directories in transfer folder
    for each experiment.

--- cli/experiment.py
+++ cli/experiment.py
@@ -3,8 +3,7 @@
 class Experiment:
     'Class used to hold information about an experiment'
     def __init__(self, name, start_date, subjects, task, set_variables = None,
-                 persistent_variables = None, folder = None, transfer_folder = None,
-                 summary_variables = None):
+                 persistent_variables = None, folder = None, summary_variables = None):
       ''' Arguments:
       subjects : dict specifying the ID of the subject run on each board.
       set_variables:  dict specifying variables whose values should be 
@@ -22,7 +21,6 @@ class Experiment:
       self.set_variables = set_variables
       self.persistent_variables = persistent_variables
       self.n_subjects = len(self.subjects.keys())
-      self.transfer_folder = transfer_folder
       self.summary_variables = summary_variables
       if folder:
         self.folder = folder
