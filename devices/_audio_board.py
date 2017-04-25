091b063019ec4c1ecefc53b8ec3950cd9d1db821
Thomas Akam <4661184+ThomasAkam@users.noreply.github.com>
2017-04-25 19:37:13 +0100

    - Modified Audio_board.set_volume() to turn of I2C between calls.

--- devices/_audio_board.py
+++ devices/_audio_board.py
@@ -3,10 +3,12 @@ from pyControl.audio import Audio_output
 
 class Audio_board(Audio_output):
     def __init__(self,  port):
-    	assert port.DAC is not None, '! Audio board needs port with DAC.'
-    	assert port.I2C is not None, '! Audio board needs port with I2C.'
-    	self.I2C = port.I2C
-    	super().__init__(port.DAC)
+        assert port.DAC is not None, '! Audio board needs port with DAC.'
+        assert port.I2C is not None, '! Audio board needs port with I2C.'
+        self.I2C = pyb.I2C(port.I2C)
+        super().__init__(port.DAC)
 
     def set_volume(self, V): # Set volume of audio output, range 0 - 127
-   		pyb.I2C(self.I2C, pyb.I2C.MASTER).mem_write(V, 46, 0)
+        self.I2C.init(pyb.I2C.MASTER)
+        self.I2C.mem_write(V, 46, 0)
+        self.I2C.deinit()
