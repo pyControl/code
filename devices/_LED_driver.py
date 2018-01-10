46ac62ea25da651c51221860f1db86d28772e1eb
takam <thomas.akam@gmail.com>
2018-01-10 14:51:17 +0000

    - Changed Stepper_motor off() method to stop()
    - Enabled pulse output by default on LED_driver.

--- devices/_LED_driver.py
+++ devices/_LED_driver.py
@@ -2,4 +2,4 @@ import pyControl.hardware as _h
 
 class LED_driver(_h.Digital_output):
     def __init__(self,  port):
-        super().__init__(port.DIO_A)
+        super().__init__(port.DIO_A, pulse_enabled=True)
