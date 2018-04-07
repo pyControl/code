17bf8ccac2f18c7097d2d11fee5f4bd481959ade
Thomas Akam <4661184+ThomasAkam@users.noreply.github.com>
2018-04-07 23:50:55 +0100

    run_task_gui:
    - Each states and event plotted with different color.
    
    - Added config and variables buttons, rearanged layout.

--- gui/run_task_gui.py
+++ gui/run_task_gui.py
@@ -15,10 +15,6 @@ from com.pycboard import Pycboard, PyboardError
 from com.data_logger import Data_logger
 from config.paths import data_dir, tasks_dir
 
-# Configure pyqtgraph.
-pg.setConfigOption('background', 'w') 
-pg.setConfigOption('foreground', 'k')
-
 # Run_task_gui ------------------------------------------------------------------------
 
 app = QtGui.QApplication([])  # Start QT
@@ -48,11 +44,13 @@ class Run_task_gui(QtGui.QWidget):
         self.port_label = QtGui.QLabel("Serial port:")
         self.port_text = QtGui.QLineEdit('com1')
         self.connect_button = QtGui.QPushButton('Connect')
+        self.config_button = QtGui.QPushButton('Config')
         self.task_label = QtGui.QLabel("Task:")
         self.task_select = QtGui.QComboBox()
         self.upload_button = QtGui.QPushButton('Upload')
+        self.variables_button = QtGui.QPushButton('Variables')
         self.data_dir_label = QtGui.QLabel("Data dir:")
-        self.data_dir_text = QtGui.QLineEdit('Select directory')
+        self.data_dir_text = QtGui.QLineEdit(data_dir)
         self.data_dir_button = QtGui.QPushButton('...')
         self.data_dir_button.setFixedWidth(30)
         self.subject_label = QtGui.QLabel("Subject ID:")
@@ -71,28 +69,33 @@ class Run_task_gui(QtGui.QWidget):
         self.vertical_layout     = QtGui.QVBoxLayout()
         self.horizontal_layout_1 = QtGui.QHBoxLayout()
         self.horizontal_layout_2 = QtGui.QHBoxLayout()
+        self.horizontal_layout_3 = QtGui.QHBoxLayout()
 
         self.horizontal_layout_1.addWidget(self.status_label)
         self.horizontal_layout_1.addWidget(self.status_text)
         self.horizontal_layout_1.addWidget(self.port_label)
         self.horizontal_layout_1.addWidget(self.port_text)
-        self.port_text.setFixedWidth(70)
         self.horizontal_layout_1.addWidget(self.connect_button)
-        self.horizontal_layout_1.addWidget(self.task_label)
-        self.horizontal_layout_1.addWidget(self.task_select)
-        self.horizontal_layout_1.addWidget(self.upload_button)
-        self.horizontal_layout_2.addWidget(self.data_dir_label)
-        self.horizontal_layout_2.addWidget(self.data_dir_text)
-        self.horizontal_layout_2.addWidget(self.data_dir_button)
-        self.horizontal_layout_2.addWidget(self.subject_label)
-        self.horizontal_layout_2.addWidget(self.subject_text)
-        self.horizontal_layout_2.addWidget(self.start_button)
-        self.horizontal_layout_2.addWidget(self.stop_button)
+        self.horizontal_layout_1.addWidget(self.config_button)
+        self.horizontal_layout_2.addWidget(self.task_label)
+        self.horizontal_layout_2.addWidget(self.task_select)
+        self.horizontal_layout_2.addWidget(self.upload_button)
+        self.horizontal_layout_2.addWidget(self.variables_button)
+        self.horizontal_layout_2.addStretch()
+        self.horizontal_layout_3.addWidget(self.data_dir_label)
+        self.horizontal_layout_3.addWidget(self.data_dir_text)
+        self.horizontal_layout_3.addWidget(self.data_dir_button)
+        self.horizontal_layout_3.addWidget(self.subject_label)
+        self.horizontal_layout_3.addWidget(self.subject_text)
+        self.horizontal_layout_3.addWidget(self.start_button)
+        self.horizontal_layout_3.addWidget(self.stop_button)
 
         self.vertical_layout.addLayout(self.horizontal_layout_1)
         self.vertical_layout.addLayout(self.horizontal_layout_2)
-        self.vertical_layout.addWidget(self.task_plot, 70)
-        self.vertical_layout.addWidget(self.log_text , 30)
+        self.vertical_layout.addLayout(self.horizontal_layout_3)
+        self.vertical_layout.addWidget(self.log_text , 20)
+        self.vertical_layout.addWidget(self.task_plot, 80)
+
 
         self.setLayout(self.vertical_layout)
 
@@ -126,8 +129,10 @@ class Run_task_gui(QtGui.QWidget):
     def connect(self):
         try:
             self.status_text.setText('Connecting...')
+            self.repaint()            
             self.board = Pycboard(self.port_text.text())
             self.connect_button.setEnabled(False)
+            self.config_button.setEnabled(True)
             self.port_text.setEnabled(False)
             self.task_select.setEnabled(True)
             self.upload_button.setEnabled(True)
@@ -139,7 +144,11 @@ class Run_task_gui(QtGui.QWidget):
         try:
             task = self.task_select.currentText()
             self.status_text.setText('Uploading..')
+            self.start_button.setEnabled(False)
+            self.variables_button.setEnabled(False)
+            self.repaint()
             self.sm_info = self.board.setup_state_machine(task)
+            self.variables_button.setEnabled(True)
             self.data_logger = Data_logger(data_dir, 'run_task', task, self.sm_info)
             self.task_plot.set_state_machine(self.sm_info)
             self.start_button.setEnabled(True)
@@ -151,10 +160,10 @@ class Run_task_gui(QtGui.QWidget):
 
     def not_connected(self):
         # Configure buttons for not connected state.
+        self.config_button.setEnabled(False)
         self.task_select.setEnabled(False)
         self.upload_button.setEnabled(False)
-        self.data_dir_text.setEnabled(False)
-        self.data_dir_button.setEnabled(False)
+        self.variables_button.setEnabled(False)
         self.subject_text.setEnabled(False)
         self.start_button.setEnabled(False)
         self.stop_button.setEnabled(False)
@@ -217,40 +226,57 @@ class Task_plotter(QtGui.QWidget):
 
         # Setup plots
 
-        self.state_plot = self.state_axis.plot(pen=pg.mkPen('k', width=3))
-        self.event_plot = self.event_axis.plot(pen=None, symbol='o', symbolSize=4, symbolPen=None)
+        self.event_plot = pg.ScatterPlotItem(size=6, pen=None)
+        self.event_axis.addItem(self.event_plot)
+
         self.event_axis.setXLink(self.state_axis)
+        self.analog_axis.setXLink(self.state_axis)
 
         # create layout
 
         self.vertical_layout = QtGui.QVBoxLayout()
-        self.vertical_layout.addWidget(self.state_axis)
-        self.vertical_layout.addWidget(self.event_axis)
-        self.vertical_layout.addWidget(self.analog_axis)
-        self.analog_axis.setVisible(False)
+        self.vertical_layout.addWidget(self.state_axis,1)
+        self.vertical_layout.addWidget(self.event_axis,1)
+        self.vertical_layout.addWidget(self.analog_axis,1)
 
         self.setLayout(self.vertical_layout)
 
+
     def set_state_machine(self, sm_info):
+        # Initialise plots with state machine information.
         self.events = sm_info['events'] # dict {event_name: ID}
         self.states = sm_info['states'] # dict {state_name: ID}
         self.analog_inputs = sm_info['analog_inputs']
-        if self.states:
-            self.state_axis.getAxis('left').setTicks([[(i, n) for (n, i) in self.states.items()]])
-            self.state_axis.setYRange(min(self.states.values()), max(self.states.values()), padding=0.2)
-            self.state_data = expanding_data_array()
+        self.n_colours = len(self.events) + len(self.states)
+        self.state_axis.clear()
+        self.event_plot.clear()
+        self.analog_axis.clear()
+        self.state_axis.getAxis('left').setTicks([[(i, n) for (n, i) in self.states.items()]])
+        self.state_axis.setYRange(min(self.states.values()), max(self.states.values()), padding=0.2)
+        self.state_plots = {ID: self.state_axis.plot(
+            pen=pg.mkPen(pg.intColor(ID, self.n_colours), width=3))
+            for ID in self.states.values()}
+        self.state_data = {ID: expanding_array() 
+                           for ID in self.states.values()}
         if self.events:
             self.event_axis.getAxis('left').setTicks([[(i, n) for (n, i) in self.events.items()]])
             self.event_axis.setYRange(min(self.events.values()), max(self.events.values()), padding=0.2)
-            self.event_data = expanding_data_array()
         if self.analog_inputs:
             self.analog_axis.setVisible(True)
+        else:
+            self.analog_axis.setVisible(False)
+
 
     def run_start(self):
         self.start_time = time.time()
+        self.event_plot.clear()
+        for state_plot in self.state_plots.values():
+            state_plot.clear()
         self.current_state = None
+        self.current_state_data = np.zeros([1,2])
 
     def process_data(self, new_data):
+        # Update plots.
         run_time = time.time() - self.start_time
         self.state_axis.setRange(xRange=[run_time-10, run_time])
         # Plot new states or events.
@@ -258,20 +284,22 @@ class Task_plotter(QtGui.QWidget):
             if nd[0] == 'D': # State entry or event.
                 if nd[2] in self.states.values(): # State entry.
                     self.current_state = nd[2]
-                    self.state_data.put(nd[1], nd[2]) # New state entry.
-                    self.state_data.put(nd[1], nd[2]) # New state exit, overwritten each update.
+                    self.state_data[nd[2]].put(nd[1], nd[2]) # New state entry.
+                    self.state_data[nd[2]].put(nd[1], nd[2]) # New state exit, overwritten each update.
+                    self.current_state_data = self.state_data[nd[2]].get()
                 else: # Event
-                    self.event_data.put(nd[1], nd[2])
-                    self.event_plot.setData(*self.event_data.get())
-        # Extend current state line.
-        self.state_data.put(int(run_time*1000), self.current_state, replace=True)
-        self.state_plot.setData(*self.state_data.get(), connect='pairs')
+                    self.event_plot.addPoints([{'pos':(nd[1]/1000, nd[2]),
+                        'brush':pg.intColor(nd[2], self.n_colours)}])
+        # Extend current state.
+        self.current_state_data[-1,0] = run_time
+        self.state_plots[self.current_state].setData(
+            *self.current_state_data.T, connect='pairs')
 
-class expanding_data_array():
 
-    def __init__(self, dtype=int):
-        self.dtype = dtype
-        self.data = np.empty([1000,2], dtype)
+class expanding_array():
+
+    def __init__(self):
+        self.data = np.empty([1000,2])
         self.i = 0
         self.len = self.data.shape[0]
 
@@ -279,21 +307,17 @@ class expanding_data_array():
         # Put new data into array expanding if necessary,
         if replace: # Replace previous sample.
             self.i -= 1
-        self.data[self.i,:] = (timestamp, ID)
+        self.data[self.i,:] = (timestamp/1000, ID)
         self.i += 1
         if self.i >= self.len: # Double array size.
             temp = self.data
-            self.data = np.empty([self.len*2,2], self.dtype)
+            self.data = np.empty([self.len*2,2])
             self.data[:self.len] = temp
             self.len =self.len*2
 
-    def get(self, seconds_timestamps=True):
+    def get(self):
         # Get all valid data from array
-        if seconds_timestamps: # Convert timestamps to seconds.
-            return (self.data[:self.i,0]/1000, self.data[:self.i,1])
-        else:
-            return (self.data[:self.i,0], self.data[:self.i,1])
-
+        return self.data[:self.i,:]
 
 # Start GUI.
 
