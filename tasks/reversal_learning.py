e47ae5aa190cf4c1dc67317b9445c6eb19092c6c
takam <4661184+ThomasAkam@users.noreply.github.com>
2018-09-26 13:26:43 +0100

    - Modified reversal learning example to better reflect real tasks.
    - Added ability to specify units to Rsync alignment code.

--- tasks/reversal_learning.py
+++ tasks/reversal_learning.py
@@ -1,7 +1,8 @@
 # A probabilistic reversal learning task in which the subject must initiate
-# the trial in the center poke, then chose left or right for a probabilistic
-# reward.  The reward probabilities on the left and right side reverse from
-# time to time.
+# the trial in the center poke, then chose left or right pokes for a 
+# probabilistic reward.  The reward probabilities on the left and right side
+# reverse once a moving average of choices crosses a threshold fraction 
+# correct, with a random delay between the treshold crossing and reversal. 
 
 from pyControl.utility import *
 import hardware_definition as hw
@@ -27,18 +28,70 @@ initial_state = 'init_trial'
 # Variables.
 #-------------------------------------------------------------------------
 
-v.session_duration = 1*hour
-v.reward_durations = [100,100] # Reward delivery duration (ms) [left, right]
-v.ITI_duration = 1*second # Inter trial interval duration.
-v.n_rewards = 0 # Number of rewards obtained.
-v.n_trials = 0 # Number of trials recieved.
-v.mean_block_length = 10 # Average block length between reversals.
-v.state = withprob(0.5) # Which side is currently good: True: left, False: right
-v.good_prob = 0.8 # Reward probabilities on the good side.
-v.bad_prob  = 0.2 # Reward probabilities on the bad side.
+# Parameters.
+v.session_duration = 1*hour      # Session duration.
+v.reward_durations = [100,100]   # Reward delivery duration (ms) [left, right].
+v.ITI_duration = 1*second        # Inter trial interval duration.
+v.threshold = 0.75               # Performance treshold used for triggering reversal.
+v.tau = 8                        # Time constant for moving average of choices (trials).
+v.trials_post_threshold = [5,15] # Trials after threshold crossing before reversal occurs [min, max].
+v.good_prob = 0.8                # Reward probabilities on the good side.
+v.bad_prob  = 0.2                # Reward probabilities on the bad side.
+
+# Variables.
+v.n_rewards = 0                  # Number of rewards obtained.
+v.n_trials = 0                   # Number of trials recieved.
+v.n_blocks = 0                   # Number of reversals.
+v.state = withprob(0.5)          # Which side is currently good: True: left, False: right
+v.mov_ave = exp_mov_ave(tau=v.tau, init_value = 0.5) # Moving average of choices.
+v.threshold_crossed = False      # Whether performance threshold has been crossed.
+v.trials_till_reversal = 0       # Used after threshold crossing to trigger reversal.
 
 #-------------------------------------------------------------------------        
-# Define behaviour.
+# Non-state machine code.
+#-------------------------------------------------------------------------
+
+def trial_outcome(choice):
+    # Function called after choice is made which determines trial outcome,
+    # controls when reversals happen, and prints trial information.
+
+    # Determine trial outcome.
+    if choice: # Subject chose left.
+        if v.state:
+            reward_prob = v.good_prob
+        else:
+            reward_prob = v.bad_prob
+    else: # Subject chose right
+        if v.state:
+            reward_prob = v.bad_prob
+        else:
+            reward_prob = v.good_prob
+    outcome = withprob(reward_prob) # Whether trial is rewarded or not.
+
+    # Determine when reversal occurs.
+    v.mov_ave.update(choice) # Update moving average of choices.
+    if v.threshold_crossed: # Subject has already crossed threshold.
+        v.trials_till_reversal -= 1
+        if v.trials_till_reversal == 0: # Trigger reversal.
+            v.state = not v.state
+            v.threshold_crossed = False
+            v.n_blocks += 1
+    else: # Check for threshold crossing.
+        if ((    v.state and (v.mov_ave.value > v.threshold)) or
+            (not v.state and (v.mov_ave.value < (1- v.threshold)))):
+            v.threshold_crossed = True
+            v.trials_till_reversal = randint(*v.trials_post_threshold)
+
+    # Print trial information.
+    v.n_trials  +=1
+    v.n_rewards += outcome
+    print('T#:{} R#:{} B#:{} C:{:d} O:{:d} S:{:d} A:{:.2f}'.format(
+           v.n_trials, v.n_rewards, v.n_blocks, choice, outcome, v.state, v.mov_ave.value))
+
+    return outcome
+
+#-------------------------------------------------------------------------        
+# State machine code.
 #-------------------------------------------------------------------------
 
 # Run start and stop behaviour.
@@ -64,51 +117,44 @@ def init_trial(event):
         goto_state('choice_state')
 
 def choice_state(event):
-    # Wait for left or right choice and evaluate if reward is delivered.
+    # Wait for left or right choice, evaluate if reward is delivered using trial_outcome function.
     if event == 'entry':
-        print('Trials: {}, Rewards:  {}'.format(v.n_trials, v.n_rewards))
         hw.left_poke.LED.on()
         hw.right_poke.LED.on()
     elif event == 'exit':
         hw.left_poke.LED.off()
         hw.right_poke.LED.off()
     elif event == 'left_poke':
-        if ((v.state and withprob(v.good_prob)) or (not v.state and withprob(v.bad_prob))):
+        if trial_outcome(True):
             goto_state('left_reward')
         else:
             goto_state('inter_trial_interval')
     elif event == 'right_poke':
-        if ((not v.state and withprob(v.good_prob)) or (v.state and withprob(v.bad_prob))):
+        if trial_outcome(False):
             goto_state('right_reward')
         else:
             goto_state('inter_trial_interval')
 
 def left_reward(event):
-    # Deliver reward to left poke, increment reward counter.
+    # Deliver reward to left poke.
     if event == 'entry':
         timed_goto_state('inter_trial_interval', v.reward_durations[0])
         hw.left_poke.SOL.on()
-        v.n_rewards += 1
     elif event == 'exit':
         hw.left_poke.SOL.off()
 
 def right_reward(event):
-    # Deliver reward to right poke, increment reward counter.
+    # Deliver reward to right poke.
     if event == 'entry':
         timed_goto_state('inter_trial_interval', v.reward_durations[1])
         hw.right_poke.SOL.on()
-        v.n_rewards += 1
     elif event == 'exit':
         hw.right_poke.SOL.off()
 
 def inter_trial_interval(event):
-    # Increment trial counter, check for reversal transition.
+    # Go to init trial after specified delay.
     if event == 'entry':
         timed_goto_state('init_trial', v.ITI_duration)
-        v.n_trials += 1
-        if withprob(1/v.mean_block_length): 
-            print('Block transition')
-            v.state = not v.state # Reversal has occured.
 
 # State independent behaviour.
 
