a1a1f52b593aa39205a6726f6bca81f675ae108a
Thomas Akam <4661184+ThomasAkam@users.noreply.github.com>
2018-10-14 15:37:43 +0100

    Main GUI.
     - Variables table updateing availibility of variables and subjects improved.
    
     - Started work on functionality to save and load experiments as JSON files.

--- config/paths.py
+++ config/paths.py
@@ -2,10 +2,11 @@ import os
 
 top_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # Top level pyControl folder.
 
-config_dir    = os.path.join(top_dir, 'config')
-framework_dir = os.path.join(top_dir, 'pyControl')
-devices_dir   = os.path.join(top_dir, 'devices')
-tasks_dir     = os.path.join(top_dir, 'tasks')
-data_dir      = os.path.join(top_dir, 'data')
-transfer_dir  = None # Folder to copy data to at end of run experiment.
+config_dir      = os.path.join(top_dir, 'config')
+framework_dir   = os.path.join(top_dir, 'pyControl')
+devices_dir     = os.path.join(top_dir, 'devices')
+tasks_dir       = os.path.join(top_dir, 'tasks')
+experiments_dir = os.path.join(top_dir, 'experiments')
+data_dir        = os.path.join(top_dir, 'data')
+transfer_dir    = None # Folder to copy data to at end of run experiment.
 
