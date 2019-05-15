31d741cc957b8d09f0cefc7e272fab4923a3466d
Jimmy Rowland <jimmy.rowland27@gmail.com>
2019-05-15 14:14:01 +0100

    init clean api-dev branch

--- config/hardware_definition.py
+++ config/hardware_definition.py
@@ -2,13 +2,15 @@
 # port 4 of breakout board version 1.2.  The houselight is plugged into the center pokes solenoid socket.
 
 from devices import *
-import pyb
-from pyControl.hardware import *
 
 board = Breakout_1_2()
 
-# Instantiate digital output connected to BNC_1.
-Lickometer = Lickometer(port=board.port_1)
-solenoid = Lickometer.SOL_1
-sync_output = Rsync(pin=board.BNC_1, mean_IPI=1000, pulse_dur=10) # Instantiate Rsync object on breakout board BNC_1
+# Instantiate Devices.
+left_poke   = Poke(board.port_1, rising_event = 'left_poke'  , falling_event = 'left_poke_out' )
+center_poke = Poke(board.port_2, rising_event = 'center_poke', falling_event = 'center_poke_out')
+right_poke  = Poke(board.port_3, rising_event = 'right_poke' , falling_event = 'right_poke_out')
 
+speaker = Audio_board(board.port_4)
+
+# Aliases
+houselight = center_poke.SOL 
