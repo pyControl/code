ac9b0bd40a58d02a3ef8b8cdfa3fa4f628e9ddaf
takam <thomas.akam@gmail.com>
2020-01-06 17:17:47 +0000

    Deleted markov example used to show private variables. (+1 squashed commits)
    Squashed commits:
    
    [39bfaa9] - Simplified code that reads varaiables names from task files in configure experiment tab.

--- gui/configure_experiment_tab.py
+++ gui/configure_experiment_tab.py
@@ -486,11 +486,8 @@ class VariablesTable(QtGui.QTableWidget):
                 file_content = file.read()
         except FileNotFoundError:
             return
-        self.variable_names = []
-        for v_name in re.findall(pattern, file_content):
-            if not v_name in [var_name for var_name in self.variable_names]:
-                if not v_name[-3:] == '___':
-                    self.variable_names.append(v_name)
+        self.variable_names = list(set([v_name for v_name in 
+            re.findall(pattern, file_content) if not v_name[-3:] == '___']))
         # Remove variables that are not in new task.
         for i in reversed(range(self.n_variables)):
             if not self.cellWidget(i,0).currentText() in self.variable_names:
