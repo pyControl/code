3494e0701664a66b5d756cbefb0372d60027dfd8
takam <thomas.akam@psy.ox.ac.uk>
2020-01-27 15:46:33 +0000

    - Fixed bug caused by not including pycboard in earlier commit which changed directory path variables.

--- com/pycboard.py
+++ com/pycboard.py
@@ -5,7 +5,7 @@ import inspect
 from serial import SerialException
 from array import array
 from .pyboard import Pyboard, PyboardError
-from config.paths import config_dir, framework_dir, devices_dir, tasks_dir
+from config.paths import dirs
 
 # ----------------------------------------------------------------------------------------
 #  Helper functions.
@@ -252,11 +252,11 @@ class Pycboard(Pyboard):
     # pyControl operations.
     # ------------------------------------------------------------------------------------
 
-    def load_framework(self, framework_dir=framework_dir):
+    def load_framework(self):
         '''Copy the pyControl framework folder to the board.'''
         self.print('\nTransfering pyControl framework to pyboard.', end='')
-        self.transfer_folder(framework_dir, file_type='py', show_progress=True)
-        self.transfer_folder(devices_dir  , file_type='py', show_progress=True)
+        self.transfer_folder(dirs['framework'], file_type='py', show_progress=True)
+        self.transfer_folder(dirs['devices']  , file_type='py', show_progress=True)
         error_message = self.reset()
         if not self.status['framework']:
             self.print('\nError importing framework:')
@@ -265,7 +265,7 @@ class Pycboard(Pyboard):
             self.print(' OK')
         return 
 
-    def load_hardware_definition(self, hwd_path=os.path.join(config_dir, 'hardware_definition.py')):
+    def load_hardware_definition(self, hwd_path=os.path.join(dirs['config'], 'hardware_definition.py')):
         '''Transfer a hardware definition file to pyboard.  Defaults to transfering 
         file hardware_definition.py from config folder.'''
         if os.path.exists(hwd_path):
@@ -282,7 +282,7 @@ class Pycboard(Pyboard):
         else:
             self.print('Hardware definition file not found.') 
 
-    def setup_state_machine(self, sm_name, sm_dir=tasks_dir, uploaded=False):
+    def setup_state_machine(self, sm_name, sm_dir=dirs['tasks'], uploaded=False):
         '''Transfer state machine descriptor file sm_name.py from folder sm_dir
         to board. Instantiate state machine object as state_machine on pyboard.'''
         self.reset()
