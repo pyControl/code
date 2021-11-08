dbc453d6db7e8c0dacb7c0a0e8b7d26edba715c4
takam <thomas.akam@psy.ox.ac.uk>
2021-11-08 20:51:18 +0000

    - Changed default font size for GUI elements to 11 from 12.

--- config/gui_settings.py
+++ config/gui_settings.py
@@ -8,5 +8,5 @@ event_history_len  = 200  # Length of event history to plot (# events).
 state_history_len  = 100  # Length of state history to plot (# states). 
 analog_history_dur = 12   # Duration of analog signal history to plot (seconds).
 
-ui_font_size  = 12  # Font size of UI elements (button text, label text etc.).
+ui_font_size  = 11  # Font size of UI elements (button text, label text etc.).
 log_font_size = 9   # Font size of the data log.
