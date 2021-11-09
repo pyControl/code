67eea59586e8ad284d9ee3bca0a2994df1269827
takam <thomas.akam@psy.ox.ac.uk>
2021-11-09 21:15:55 +0000

    - Modified plotting classes so that they no longer use setPos to scroll the plot and instead implement the scrolling by modifying the plotted data.  The previous method no longer worked in pyqtgraph versions > 0.11.0.
    - Changed the default update interval for the GUI to 10ms to give smoother plot scrolling at the cost of somewhat higher CPU usage.

--- config/gui_settings.py
+++ config/gui_settings.py
@@ -2,7 +2,7 @@
 
 VERSION = '1.6'
 
-update_interval = 20 # Interval between calls to the GUIs update function (ms).
+update_interval = 10 # Interval between calls to the GUIs update function (ms).
 
 event_history_len  = 200  # Length of event history to plot (# events).
 state_history_len  = 100  # Length of state history to plot (# states). 
