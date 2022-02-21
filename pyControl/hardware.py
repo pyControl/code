cb6af2da0fa8585d43c47943a88921102719e623
mostafa.safaie <mostafa.safaie@gmail.com>
2022-02-21 15:16:43 +0000

    add more condition for `get_analog_inputs`

--- pyControl/hardware.py
+++ pyControl/hardware.py
@@ -93,7 +93,7 @@ def off():
 def get_analog_inputs():
     # Print dict of analog inputs {name: {'ID': ID, 'Fs':sampling rate}}
     print({io.name:{'ID': io.ID, 'Fs': io.sampling_rate}
-          for io in IO_dict.values() if hasattr(io, 'recording')})
+          for io in IO_dict.values() if hasattr(io, 'recording') and hasattr(io, 'sampling_rate')})
 
 # IO_object -------------------------------------------------------------------
 
