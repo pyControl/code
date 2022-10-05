d363b939fb090e523f26cd50cbf0ed26f07b7f8d
takam <thomas.akam@psy.ox.ac.uk>
2022-10-05 15:59:34 +0100

    Added device driver file providing basic functionality for reading RFID tags using Priority 1 Design Micro RFID module

---
+++ devices/uRFID.py
@@ -0,0 +1,24 @@
+from pyb import UART
+from time import sleep
+
+class uRFID:
+    # Class for using the Priority 1 Design Micro RFID module to read FDX-B tags.
+    # http://www.priority1design.com.au/rfid_reader_modules.html
+
+    def __init__(self, port):
+        self.uart = UART(port.UART)
+        self.uart.init(baudrate=9600, bits=8, parity=None, stop=1, timeout=1)
+        self.uart.write(b'ST2\r') # Put reader in FDX-B tag mode.
+        sleep(0.01)
+        self.uart.read() # Clear input buffer.
+
+    def read_tag(self):
+        # Return the ID of the most recent tag read, if no tag has been read return None.
+        read_bytes = self.uart.read()
+        if not read_bytes:
+            return
+        try:
+            ID = int(read_bytes[-13:-1])
+            return ID
+        except ValueError:
+            return
