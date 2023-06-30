24cb3cd3ee8d218f7435dc852cf673eff4228ee4
Andy Lustig <alustig3@gmail.com>
2023-06-29 22:05:42 -0600

    reorder states for more intuitive states plotting

--- tasks/example/custom_controls_dialog.py
+++ tasks/example/custom_controls_dialog.py
@@ -12,7 +12,7 @@ red_LED = LED(1)
 green_LED = LED(2)
 
 # States and events.
-states = ["green_off", "red_off", "green_on", "red_on"]
+states = ["green_off", "green_on", "red_off", "red_on"]
 events = ["green_off_wait", "red_off_wait"]
 
 initial_state = "red_off"
@@ -28,8 +28,8 @@ v.current_count___ = 0
 
 # Custom controls dialog declaration
 # Uncomment 1 of the 3 below. (files found in config/user_controls_dialogs/)
-v.custom_controls_dialog = "blinker_gui"  # example dialog 
-# v.custom_controls_dialog = 'blinker_gui_tabs' # example dialog with tabs 
+v.custom_controls_dialog = "blinker_gui"  # example dialog
+# v.custom_controls_dialog = 'blinker_gui_tabs' # example dialog with tabs
 # v.custom_controls_dialog = 'blinker_gui_from_py' # advanced example dialog that is loaded from a .py file
 
 
