704a6c394c257d0b53d764cc3fa276e60876c53d
Andy Lustig <alustig3@gmail.com>
2025-04-04 17:01:59 -0400

    updated running_wheel.py example
    - uses new syntax for adding trigger
    - demonstrates ability to have multiple triggers

--- tasks/example/running_wheel.py
+++ tasks/example/running_wheel.py
@@ -1,9 +1,12 @@
 # Example of using a rotary encoder to measure running speed and trigger events when
 # running starts and stops. The subject must run for 10 seconds to trigger reward delivery,
 # then stop running for 5 seconds to initiate the next trial.
+# If while running the subject exceeds a bonus velocity threshold, they earn a bonus
+# and the reward duration is extended by a bonus duration.
 
 from pyControl.utility import *
 from devices import *
+from pyControl.hardware import Analog_threshold
 
 # Variables.
 
@@ -11,7 +14,20 @@ v.run_time = 10 * second  # Time subject must run to obtain reward.
 v.stop_time = 5 * second  # Time subject must stop running to intiate the next trial.
 v.reward_duration = 100 * ms  # Time reward solenoid is open for.
 v.velocity_threshold = 100  # Minimum encoder velocity treated as running (encoder counts/second).
+v.bonus_velocity_threshold = 5000  # Encoder velocity that triggers bonus reward (encoder counts/second).
+v.give_bonus = False  # Whether to give bonus reward.
+v.bonus_reward_duration = 50 * ms  # Time to add to reward duration if bonus is earned.
 
+running_trigger = Analog_threshold(
+    threshold=v.velocity_threshold,
+    rising_event="started_running",
+    falling_event="stopped_running",
+)
+
+bonus_trigger = Analog_threshold(
+    threshold=v.bonus_velocity_threshold,
+    rising_event="bonus_earned",
+)
 # Instantiate hardware - would normally be in a seperate hardware definition file.
 
 board = Breakout_1_2()  # Breakout board.
@@ -21,9 +37,7 @@ running_wheel = Rotary_encoder(
     name="running_wheel",
     sampling_rate=100,
     output="velocity",
-    threshold=v.velocity_threshold,
-    rising_event="started_running",
-    falling_event="stopped_running",
+    triggers=[running_trigger, bonus_trigger],
 )  # Running wheel must be plugged into port 1 of breakout board.
 
 solenoid = Digital_output(board.port_2.POW_A)  # Reward delivery solenoid.
@@ -40,6 +54,7 @@ states = [
 events = [
     "started_running",
     "stopped_running",
+    "bonus_earned",
     "run_timer",
     "stopped_timer",
     "reward_timer",
@@ -70,10 +85,13 @@ def running_for_reward(event):
     # If subject runs for long enough go to reward state.
     # If subject stops go back to trial start.
     if event == "entry":
+        v.give_bonus = False
         set_timer("run_timer", v.run_time)
     elif event == "stopped_running":
         disarm_timer("run_timer")
         goto_state("trial_start")
+    elif event == "bonus_earned":
+        v.give_bonus = True
     elif event == "run_timer":
         goto_state("reward")
 
@@ -81,7 +99,7 @@ def running_for_reward(event):
 def reward(event):
     # Deliver reward then go to inter trial interval.
     if event == "entry":
-        timed_goto_state("inter_trial_interval", v.reward_duration)
+        timed_goto_state("inter_trial_interval", v.reward_duration + v.bonus_reward_duration * v.give_bonus)
         solenoid.on()
     elif event == "exit":
         solenoid.off()
