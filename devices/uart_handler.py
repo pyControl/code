7a0d1b9239e9e53410ab2047b9bd5bb7564c6447
Andy Lustig <alustig3@gmail.com>
2025-08-26 14:52:33 -0400

    cleaned up uart_handler code

--- devices/uart_handler.py
+++ devices/uart_handler.py
@@ -6,12 +6,12 @@ class UART_handler(IO_object):
     This class is used to generate a framework event when a UART message is received.
     """
 
-    def __init__(self, event_name, first_char_interrupt=True):
+    def __init__(self, event_name, mcu="STM32_F4"):
         self.event_name = event_name
         self.last_interrupt_time = 0
-        self.first_char_interrupt = first_char_interrupt
-        if self.first_char_interrupt:
-            self.msg_starting = False
+        self.mcu = mcu
+        if self.mcu == "STM32_F4":
+            self.accept_interrupt = True
         assign_ID(self)
 
     def _initialise(self):
@@ -23,13 +23,14 @@ class UART_handler(IO_object):
             #   AND at the end when the RX is idle.
             # - pyboard D-series board (STM32F7): IRQ_RXIDLE interrupt is triggered ONLY at the end when the RX is idle
             # - see Micropytyhon UART docs for more info: https://docs.micropython.org/en/latest/library/machine.UART.html
-            if self.first_char_interrupt:
-                self.msg_starting = not self.msg_starting
-                if self.msg_starting:
-                    # ignore the first interrupt after the message starts (when using pyboard v1.1)
-                    return
-            self.timestamp = fw.current_time
-            interrupt_queue.put(self.ID)
+            if self.mcu == "STM32_F4":  # respond to every other interrupt when using pyboard v1.1
+                if self.accept_interrupt:
+                    self.timestamp = fw.current_time
+                    interrupt_queue.put(self.ID)
+                self.accept_interrupt = not self.accept_interrupt
+            elif self.mcu == "STM32_F7":  # respond to every interrupt when using pyboard D-series
+                self.timestamp = fw.current_time
+                interrupt_queue.put(self.ID)
 
     def _process_interrupt(self):
         fw.event_queue.put(fw.Datatuple(self.timestamp, fw.EVENT_TYP, "i", self.event_ID))
