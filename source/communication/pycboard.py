0e7c6683d45658c45ddbaaeb2eb00a7332078706
Andy Lustig <alustig3@gmail.com>
2025-10-30 10:22:57 -0500

    fixes bug where board would disconnect if opening controls before starting task

--- source/communication/pycboard.py
+++ source/communication/pycboard.py
@@ -421,6 +421,7 @@ class Pycboard(Pyboard):
             micropython_version=self.micropython_version,
         )
         self.data_logger.reset()
+        self.timestamp = 0
 
     def get_states(self):
         """Return states as a dictionary {state_name: state_ID}"""
@@ -440,7 +441,6 @@ class Pycboard(Pyboard):
         self.exec("fw.data_output = " + repr(data_output))
         self.serial.reset_input_buffer()
         self.last_message_time = time.time()
-        self.timestamp = 0
         self.exec_raw_no_follow("fw.run()")
         self.framework_running = True
 
